f9d544320b5f5a886083c91036b73b386e01a197
vojjta <ota.sverma@gmail.com>
2024-01-03 06:34:00 +0100

    Feature/init project files (#1)
    * added flutter project files
    
    * added base folder structure

---
+++ android/app/src/main/kotlin/cz/appwizards/jay/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package cz.appwizards.jay.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'presentation/navigation/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFFC02222),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 20),
+          ),
+        ),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFFC02222),
+          inversePrimary: Color(0xFFA83434),
+          onPrimary: Colors.blue,
+          secondary: Colors.orange,
+          background: Color(0xFF4b4a4a),
+          onBackground: Colors.white,
+          tertiary: Colors.deepPurple,
+          surface: Color(0xFFA83434),
+          onSurface: Colors.black,
+          surfaceVariant: Colors.white,
+        ),
+        useMaterial3: true,
+      ),
+      builder: (context, child) {
+        return Material(
+          child: child,
+        );
+      },
+      routerConfig: Routes.create(),
+    );
+  }
+}
