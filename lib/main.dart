47967ca9e4246b9a6b96f4a4d9ebfba17b6f21cd
vojjta <ota.sverma@gmail.com>
2024-01-08 07:55:28 +0100

    feat(Firebase): Init firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'presentation/navigation/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
