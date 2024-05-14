4cc75eaaec5c1835d2234450dde18aa8a9754ccd
ales <vorwerick@email.cz>
2024-05-14 14:35:52 +0200

    fix: ios config

--- lib/infrastructure/shared/info_plus_device_information_factory.dart
+++ lib/infrastructure/shared/info_plus_device_information_factory.dart
@@ -1,12 +1,17 @@
+
 import 'package:app/application/extensions/l.dart';
 import 'package:app/application/shared/device_information.dart';
 import 'package:app/application/shared/device_information_factory.dart';
 import 'package:app/infrastructure/shared/info_plus_device_information.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-final class InfoPlusDeviceInformationFactory with L implements DeviceInformationFactory {
+final class InfoPlusDeviceInformationFactory
+    with L
+    implements DeviceInformationFactory {
   DeviceInformation? _deviceInformation;
 
   @override
@@ -16,22 +21,36 @@ final class InfoPlusDeviceInformationFactory with L implements DeviceInformation
       return _deviceInformation!;
     }
 
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
 
     String? token = await FirebaseMessaging.instance.getToken();
 
-    _deviceInformation = InfoPlusDeviceInformation(
-      device: androidInfo.model,
-      manufacturer: androidInfo.manufacturer,
-      version: packageInfo.version,
-      sdk: androidInfo.version.sdkInt,
-      buildNumber: int.parse(packageInfo.buildNumber),
-      firebaseToken: token ?? '',
-    );
-    l.d('Created new device information.');
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+
+      _deviceInformation = InfoPlusDeviceInformation(
+        device: iosInfo.model,
+        manufacturer: iosInfo.identifierForVendor.toString(),
+        version: packageInfo.version,
+        sdk: 17,
+        buildNumber: int.parse(packageInfo.buildNumber),
+        firebaseToken: token ?? '',
+      );
+      l.d('Created new device information.');
+    } else {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+
+      _deviceInformation = InfoPlusDeviceInformation(
+        device: androidInfo.model,
+        manufacturer: androidInfo.manufacturer,
+        version: packageInfo.version,
+        sdk: androidInfo.version.sdkInt,
+        buildNumber: int.parse(packageInfo.buildNumber),
+        firebaseToken: token ?? '',
+      );
+      l.d('Created new device information.');
+    }
     return _deviceInformation!;
   }
 }
